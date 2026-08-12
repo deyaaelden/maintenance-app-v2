fd6d5acb0ee1ab49e1a89a9f1109180fdb3ac320
deyaaelden <deyaaelden@gmail.com>
2026-08-12 23:13:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], base: './' })
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    chunkSizeWarningLimit: 1000, // رفع الحد من 500 كيلوبايت إلى 1000 (الافتراضي 500)
+  },
+})
